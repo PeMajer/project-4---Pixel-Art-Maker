e3466ba3a314805f14f4c43a12e32e5f4eb15fbf
PeMajer <majer.petr@gmail.com>
2017-11-27 21:19:57 +0100

    Tvorba mrizky

--- designs.js
+++ designs.js
@@ -3,6 +3,26 @@
 
 // When size is submitted by the user, call makeGrid()
 
+function makeGrid() {
+	let canvasWidth = $('#input_width').val();
+	let canvasHeight = $('#input_height').val();
+
+	$('#pixel_canvas').html(' ');  //Delete exist canvas
+
+	for (let i = 0; i < canvasHeight ; i++) {
+		$('#pixel_canvas').append('<tr></tr>');
+		for (let j = 0; j < canvasWidth; j++) {
+			$('#pixel_canvas tr').last().append('<td></td>');
+		}
+	}
+}
+
+
+
+$('input').click(function(evt){
+	evt.preventDefault();
+	makeGrid();
+});
 
 
 
