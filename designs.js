4c312a8da8e13cc20dc2104046a52e781e82236e
PeMajer <majer.petr@gmail.com>
2017-11-29 13:49:35 +0100

    JS - pridani a uprava komentaru

--- designs.js
+++ designs.js
@@ -1,16 +1,16 @@
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
+
 function makeGrid() {
 	const canvasWidth = $('#input_width').val();
 	const canvasHeight = $('#input_height').val();
 
 	$('#pixel_canvas').html('');  //Delete existing canvas
 
-	for (let i = 0; i < canvasHeight ; i++) {
+	for (let i = 0; i < canvasHeight ; i++) {		//loop creating rows of grid
 		$('#pixel_canvas').append('<tr></tr>');
-		for (let j = 0; j < canvasWidth; j++) {
+		for (let j = 0; j < canvasWidth; j++) {		// loop creating columns of grid 
 			$('#pixel_canvas tr').last().append('<td></td>');
 		}
 	}
@@ -18,13 +18,13 @@ function makeGrid() {
 
 makeGrid();
 
-$(':submit').click(function(evt){  
-	evt.preventDefault();
+$(':submit').click(function(evt){	//listen for (click) event on submition button 
+	evt.preventDefault();          	//cancel default action  
 	makeGrid();
 });
 
 
-$('table').on('click','td', function(evt){
-	const color = $('#colorPicker').val();
-	$(evt.target).css('background-color', color);
+$('table').on('click','td', function(evt){   		//listen for event on table cells
+	const color = $('#colorPicker').val();			//read the value of color
+	$(evt.target).css('background-color', color);	//change color of table cell
 });
